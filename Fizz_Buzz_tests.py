516052831bf9b45e47b7d8f34095ce9a105e968e
Sam Watson <SWatson@spartaglobal.com>
2019-12-04 16:01:40 +0000

    Testing fizz buzz

---
+++ Fizz_Buzz_tests.py
@@ -0,0 +1,12 @@
+from Fizz_Buzz import *
+print('Testing Fizz Buzz')
+print('Testing 5 --> Expecting Fizz')
+print(fizz_buzz(5) == 'Fizz')
+
+print('Testing Fizz Buzz')
+print('Testing 6 --> Expecting Buzz')
+print(fizz_buzz(6) == 'Bizz')
+
+print('Testing Fizz Buzz')
+print('Testing 15 --> Expecting Bizzuu')
+print(fizz_buzz(15) == 'Bizzuu')
